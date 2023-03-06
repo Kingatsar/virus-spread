c99a0b646e21debea67b669cfd78191c26aab7a9
kingatsa <jcribaira@outlook.com>
2023-03-06 16:36:50 +0100

    add cone

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { wmtsLayer } from "./models/wmts";
 import { elevationLayer } from "./models/elevation";
 import { buildingLayer } from "./models/building";
 
-
 // View
 const viewerDiv = document.getElementById('viewerDiv');
 const placement = {
@@ -36,4 +35,40 @@ const geometry_layer = buildingLayer('http://wxs.ign.fr/3ht7xcw6f7nciopo16etuqp2
 view.addLayer(geometry_layer);
 
 
+function addMeshToScene() {
+    // creation of the new mesh (a cylinder)
+    const THREE = itowns.THREE;
+    const geometry = new THREE.CylinderGeometry(0, 10, 60, 8);
+    const material = new THREE.MeshBasicMaterial({ color: 0xffc0cb });
+    const mesh = new THREE.Mesh(geometry, material);
+
+    // get the position on the globe, from the camera
+    const cameraTargetPosition = view.controls.getLookAtCoordinate();
+
+    // position of the mesh
+    const meshCoord = cameraTargetPosition;
+    meshCoord.altitude += 30;
+
+    // position and orientation of the mesh
+    mesh.position.copy(meshCoord.as(view.referenceCrs));
+    // mesh.lookAt(new THREE.Vector3(0, 0, 0));
+    mesh.rotateX(Math.PI / 2);
+
+    // update coordinate of the mesh
+    mesh.updateMatrixWorld();
+
+    // add the mesh to the scene
+    view.scene.add(mesh);
+
+    // make the object usable from outside of the function
+    view.mesh = mesh;
+    view.notifyChange();
+}
+
+// Listen for globe full initialisation event
+view.addEventListener(itowns.GLOBE_VIEW_EVENTS.GLOBE_INITIALIZED, function globeInitialized() {
+    // eslint-disable-next-line no-console
+    console.info('Globe initialized');
+    addMeshToScene();
+});
 
--- src/models/building.js
+++ src/models/building.js
@@ -14,6 +14,7 @@ export function buildingLayer(serverURL, nameType, crs, zoomMinLayer) {
                 base_altitude: setAltitude,
                 extrusion_height: setExtrusion,
             },
+
         }),
     });
 
