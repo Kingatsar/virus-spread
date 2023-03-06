0da1e7702dd08e07e5ca5a4eb94df736c597cb3d
kingatsa <jcribaira@outlook.com>
2023-03-06 13:37:17 +0100

    function models

--- src/main.js
+++ src/main.js
@@ -1,117 +1,39 @@
+import { wmtsLayer } from "./models/wmts";
+import { elevationLayer } from "./models/elevation";
+import { buildingLayer } from "./models/building";
 
-// ----------------- Preparing the field ----------------- //
+
 // View
-var viewerDiv = document.getElementById('viewerDiv');
-var placement = {
+const viewerDiv = document.getElementById('viewerDiv');
+const placement = {
     coord: new itowns.Coordinates('EPSG:4326', 4.818, 45.7354),
     range: 1000,
     tilt: 20,
 };
-var view = new itowns.GlobeView(viewerDiv, placement);
+let view = new itowns.GlobeView(viewerDiv, placement);
 
-// Layer
-var colorSource = new itowns.WMTSSource({
-    url: 'http://wxs.ign.fr/3ht7xcw6f7nciopo16etuqp2/geoportail/wmts',
-    crs: 'EPSG:3857',
-    name: 'ORTHOIMAGERY.ORTHOPHOTOS',
-    tileMatrixSet: 'PM',
-    format: 'image/jpeg'
-});
-
-var colorLayer = new itowns.ColorLayer('Ortho', {
-    source: colorSource,
-});
-
-view.addLayer(colorLayer);
+// WMTS Layer
+const wmts_layer = wmtsLayer('http://wxs.ign.fr/3ht7xcw6f7nciopo16etuqp2/geoportail/wmts',
+    'EPSG:3857',
+    'ORTHOIMAGERY.ORTHOPHOTOS',
+    'PM',
+    'image/jpeg');
+view.addLayer(wmts_layer);
 
 // Elevation
-var elevationSource = new itowns.WMTSSource({
-    url: 'http://wxs.ign.fr/3ht7xcw6f7nciopo16etuqp2/geoportail/wmts',
-    crs: 'EPSG:4326',
-    name: 'ELEVATION.ELEVATIONGRIDCOVERAGE.HIGHRES',
-    tileMatrixSet: 'WGS84G',
-    format: 'image/x-bil;bits=32',
-    tileMatrixSetLimits: {
-        11: {
-            minTileRow: 442,
-            maxTileRow: 1267,
-            minTileCol: 1344,
-            maxTileCol: 2683
-        },
-        12: {
-            minTileRow: 885,
-            maxTileRow: 2343,
-            minTileCol: 3978,
-            maxTileCol: 5126
-        },
-        13: {
-            minTileRow: 1770,
-            maxTileRow: 4687,
-            minTileCol: 7957,
-            maxTileCol: 10253
-        },
-        14: {
-            minTileRow: 3540,
-            maxTileRow: 9375,
-            minTileCol: 15914,
-            maxTileCol: 20507
-        }
-    }
-});
+const elevation_layer = elevationLayer('http://wxs.ign.fr/3ht7xcw6f7nciopo16etuqp2/geoportail/wmts',
+    'EPSG:4326',
+    'ELEVATION.ELEVATIONGRIDCOVERAGE.HIGHRES',
+    'WGS84G',
+    'image/x-bil;bits=32');
+view.addLayer(elevation_layer);
 
-var elevationLayer = new itowns.ElevationLayer('MNT_WORLD', {
-    source: elevationSource,
-});
+// Geometry Layer 
+const geometry_layer = buildingLayer('http://wxs.ign.fr/3ht7xcw6f7nciopo16etuqp2/geoportail/wfs?',
+    'BDTOPO_BDD_WLD_WGS84G:bati_indifferencie',
+    'EPSG:4326',
+    14);
+view.addLayer(geometry_layer);
 
-view.addLayer(elevationLayer);
-
-// ----------------- Adding a GeometryLayer ----------------- //
-
-/* Properties example:
-            geometry_name: "the_geom"
-            hauteur: 9
-            id: "bati_indifferencie.19138409"
-            origin_bat: "Cadastre"
-            prec_alti: 5
-            prec_plani: 2.5
-            z_max: 83.7
-            z_min: 83.7
-*/
-
-// Coloring the data
-function setColor(properties) {
-    return new itowns.THREE.Color(0xaaaaaa);
-}
-
-// Extruding the data 
-function setExtrusion(properties) {
-    return properties.hauteur;
-}
-
-// Placing the data on the ground
-function setAltitude(properties) {
-    return properties.z_min - properties.hauteur;
-}
-
-// Instantiate the data source
-var geometrySource = new itowns.WFSSource({
-    url: 'http://wxs.ign.fr/3ht7xcw6f7nciopo16etuqp2/geoportail/wfs?',
-    typeName: 'BDTOPO_BDD_WLD_WGS84G:bati_indifferencie',
-    crs: 'EPSG:4326',
-});
-
-// create our FeatureGeometryLayer
-var geometryLayer = new itowns.FeatureGeometryLayer('Buildings', {
-    source: geometrySource,
-    zoom: { min: 14 },
-    style: new itowns.Style({
-        fill: {
-            color: setColor,
-            base_altitude: setAltitude,
-            extrusion_height: setExtrusion,
-        },
-    }),
-});
-view.addLayer(geometryLayer);
 
 
---
+++ src/models/wmts.js
@@ -0,0 +1,16 @@
+
+export function wmtsLayer(serverURL, crs, name, tileMatrixSet, format) {
+    const colorSource = new itowns.WMTSSource({
+        url: serverURL,
+        crs: crs,
+        name: name,
+        tileMatrixSet: tileMatrixSet,
+        format: format
+    });
+
+    const colorLayer = new itowns.ColorLayer('Ortho', {
+        source: colorSource,
+    });
+
+    return colorLayer;
+}
