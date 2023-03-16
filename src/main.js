4fb519e32a61c20fa8fb37dd406cc3a36ce49f22
kingatsa <jcribaira@outlook.com>
2023-03-16 17:50:48 +0100

    cones moving along path

--- src/main.js
+++ src/main.js
@@ -43,51 +43,59 @@ const layerCoord = buildingLayer('http://wxs.ign.fr/3ht7xcw6f7nciopo16etuqp2/geo
     'BDTOPO_BDD_WLD_WGS84G:bati_indifferencie',
     'EPSG:4326',
     14,
-    extent);
+    extent,
+    view);
 //  GeometryLayer
 const geometry_layer = layerCoord.layer
-const geometry_coord = layerCoord.coords
-console.log(geometry_coord)
+const ListMesh = layerCoord.coords // List Mesh
+console.log(ListMesh);
 
 
 view.addLayer(geometry_layer);
 
-function addMeshToScene() {
-    // creation of the new mesh (a cylinder)
-    const THREE = itowns.THREE;
-    const geometry = new THREE.CylinderGeometry(0, 10, 60, 8);
-    const material = new THREE.MeshBasicMaterial({ color: 0xffc0cb });
-    const mesh = new THREE.Mesh(geometry, material);
 
-    // get the position on the globe, from the camera
-    const cameraTargetPosition = view.controls.getLookAtCoordinate();
+function updateAgent(ListMesh) {
+    Object.entries(ListMesh).forEach(function ([key, val]) {
 
-    // position of the mesh
-    const meshCoord = cameraTargetPosition;
-    meshCoord.altitude += 30;
 
-    // position and orientation of the mesh
-    mesh.position.copy(meshCoord.as(view.referenceCrs));
-    // mesh.lookAt(new THREE.Vector3(0, 0, 0));
-    mesh.rotateX(Math.PI / 2);
+        const cameraTargetPosition = view.controls.getLookAtCoordinate();
 
-    // update coordinate of the mesh
-    mesh.updateMatrixWorld();
+        // position of the mesh
+        const meshCoord = cameraTargetPosition;
 
-    // add the mesh to the scene
-    view.scene.add(mesh);
 
-    // make the object usable from outside of the function
-    view.mesh = mesh;
-    view.notifyChange();
+        val.position.x += 0.1;
+        val.position.y += 0.1;
+
+        // update coordinate of the mesh
+        val.updateMatrixWorld();
+
+
+        { {/*  view.camera.camera3D.position.x += 0.01;  */ } }
+
+
+    }
+
+    )
 }
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    updateAgent(ListMesh)
+    view.mainLoop.gfxEngine.renderer.render(view.scene, view.camera.camera3D)
+}
+
+animate();
+
 
 // Listen for globe full initialisation event
 view.addEventListener(itowns.GLOBE_VIEW_EVENTS.GLOBE_INITIALIZED, function globeInitialized() {
     // eslint-disable-next-line no-console
     console.info('Globe initialized');
-    addMeshToScene();
+
+
 });
 
 
-console.log("listCoords")
+
--- src/models/building.js
+++ src/models/building.js
@@ -1,5 +1,5 @@
 
-export function buildingLayer(serverURL, nameType, crs, zoomMinLayer, extent) {
+export function buildingLayer(serverURL, nameType, crs, zoomMinLayer, extent, view) {
     const geometrySource = new itowns.WFSSource({
         url: serverURL,
         typeName: nameType,
@@ -7,7 +7,7 @@ export function buildingLayer(serverURL, nameType, crs, zoomMinLayer, extent) {
         extent: extent
     });
 
-    var listCoords = [];
+    let ListMesh = {};
 
 
     const geomLayer = new itowns.FeatureGeometryLayer('Buildings', {
@@ -21,13 +21,41 @@ export function buildingLayer(serverURL, nameType, crs, zoomMinLayer, extent) {
             },
 
         }),
-        onMeshCreated: (obj, ctx) => listCoords.push({ x: ctx.geometryLayer.builder.tmp.coords[0].x, z: ctx.geometryLayer.builder.tmp.coords[0].z, y: ctx.geometryLayer.builder.tmp.coords[0].y }),
+        onMeshCreated: function virusspread(mesh) {
+            mesh.children.forEach(c => {
 
+                let geoms = c.children[0].children[0].children[0].feature.geometries
+
+
+
+                let count = 0;
+                geoms.map(goem => {
+                    count++;
+
+                    if ((count % 70) == 0) {
+
+                        mesh = addMeshToScene((goem.properties.bbox[0] + goem.properties.bbox[2]) / 2, (goem.properties.bbox[1] + goem.properties.bbox[3]) / 2, view);
+                        let id = goem.properties.id;
+                        console.log(mesh);
+                        ListMesh[id] = mesh;
+                        { {/*  console.log(mesh)  */ } }
+
+                    }
+
+
+                })
+
+
+
+            })
+
+
+        },
     });
 
     // console.log(listCoords)
 
-    return { layer: geomLayer, coords: listCoords };
+    return { layer: geomLayer, coords: ListMesh };
 }
 
 // Coloring the data
@@ -56,3 +84,39 @@ function setAltitude(properties) {
             z_max: 83.7
             z_min: 83.7
 */
+
+
+function addMeshToScene(x, y, view) {
+    // creation of the new mesh (a cylinder)
+    const THREE = itowns.THREE;
+    const geometry = new THREE.CylinderGeometry(0, 10, 60, 8);
+    const material = new THREE.MeshBasicMaterial({ color: 0xffc0cb });
+    const mesh = new THREE.Mesh(geometry, material);
+
+    // get the position on the globe, from the camera
+    const cameraTargetPosition = view.controls.getLookAtCoordinate();
+    // const cameraTargetPosition = new itowns.Coordinates('EPSG:4326', x, y, z *******
+    // position of the mesh
+    const meshCoord = cameraTargetPosition;
+    meshCoord.altitude += 30;
+
+    meshCoord.x = x;
+    meshCoord.y = y;
+
+    // position and orientation of the mesh
+    mesh.position.copy(meshCoord.as(view.referenceCrs)); // *****
+    // mesh.lookAt(new THREE.Vector3(0, 0, 0));
+    mesh.rotateX(Math.PI / 2);
+
+    // update coordinate of the mesh
+    mesh.updateMatrixWorld(); // *****
+
+    // add the mesh to the scene
+    view.scene.add(mesh);
+
+    // make the object usable from outside of the function
+    view.mesh = mesh;
+    view.notifyChange();
+
+    return mesh
+}
